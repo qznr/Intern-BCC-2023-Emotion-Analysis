74b2f966b8ac0e5fd72698446ecc70b76935a7c8
qznr <mgustav.penting@gmail.com>
2023-03-17 02:49:32 +0700

    Rf final done

--- rfTunedTrain.py
+++ rfTunedTrain.py
@@ -1,30 +1,19 @@
 import os
 from Tfidf import y_test
-from Sampling import *
+from DatasetsLoad import datasets_test
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import precision_score
-
-# Define the list of datasets and their corresponding names
-datasets_test = [
-    (X_train_adasyn_negations, y_train_adasyn_negations, y_test, "ADASYN Negations"),
-    (X_train_adasyn_clean, y_train_adasyn_clean, y_test, "ADASYN Clean"),
-    (x_train_vec_negations, y_train_clean, y_test, "Original Negations"),
-    (x_train_vec_clean, y_train_clean, y_test, "Original Clean"),
-    (X_train_ncr_negations, y_train_ncr_negations, y_test, "NCR Negations"),
-    (X_train_ncr_clean, y_train_ncr_clean, y_test, "NCR Clean"),
-    (X_train_nm_negations, y_train_nm_negations, y_test, "NearMiss Negations"),
-    (X_train_nm_clean, y_train_nm_clean, y_test, "NearMiss Clean")
-]
 
 # Define the path to the hyperparameter files
 hyperparameter_path = "Hyperparameter Tuning"
 
 # Initialize an empty list to store the precision scores for each dataset
-precision_scores = {dataset[3]: [] for dataset in datasets_test}
+precision_scores = {}
 
 # Loop over each dataset and hyperparameter file
 for dataset in datasets_test:
-    X_train, y_train, X_test, dataset_name = dataset
+    X_train, y_train, X_test, y_test, dataset_name = dataset
+    precision_scores[dataset_name] = []  # Initialize an empty list for the current dataset
     for filename in os.listdir(hyperparameter_path):
         if filename.endswith(".txt"):
             # Extract the model and dataset names from the filename
@@ -54,9 +43,10 @@ for dataset in datasets_test:
 
                 # Evaluate the model on the test set
                 y_pred = model.predict(X_test)
-                precision = precision_score(y_test, y_pred)
+                precision = precision_score(y_test, y_pred, average='macro')
                 precision_scores[dataset_name].append(precision)
 
 # Print the precision scores for each dataset
-for dataset_name, scores in precision_scores.items():
-    print(f"{dataset_name} - Precision scores: {scores}")
+with open("Rf_final_scores.txt", "w") as f:
+    for dataset_name, scores in precision_scores.items():
+        f.write(f"{dataset_name} - Precision scores: {scores}\n")
