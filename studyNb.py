1c46df66b227a220301dd9b2b20b7cc3d42dc347
qznr <mgustav.penting@gmail.com>
2023-03-16 22:26:46 +0700

    added requirements

--- studyNb.py
+++ studyNb.py
@@ -15,4 +15,3 @@ if __name__ == '__main__':
     for x_train, y_train, x_val, y_val, dataset_name in datasets:
         # Create Optuna study for each datasets with MySQL storage
         study = optuna.create_study(study_name=f"nb_{dataset_name}", storage=optuna.storages.RDBStorage(url='mysql://myuser:mypassword@localhost/optuna_nb'), load_if_exists=True)
-
