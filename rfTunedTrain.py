321bc374806d487a651769ff572fda09bc8122a2
qznr <mgustav.penting@gmail.com>
2023-03-17 01:56:12 +0700

    Added tqdm

--- rfTunedTrain.py
+++ rfTunedTrain.py
@@ -14,17 +14,7 @@ datasets = [
     ('X_train_nm_clean', 'y_train_nm_clean', 'x_val_vec_clean', 'y_test', "NearMiss Clean")
     ]
 
-def objective(trial, x_train, y_train, x_val, y_val):
-    rf_n_estimators = trial.suggest_int("n_estimators", 10, 1000)
-    rf_max_depth = trial.suggest_int("max_depth", 2, 32, log=True)
-    rf_min_samples_split = trial.suggest_int("min_samples_split", 2, 20)
-    rf_min_samples_leaf = trial.suggest_int("min_samples_leaf", 1, 20)
-    rf_model = RandomForestClassifier(n_estimators=rf_n_estimators, max_depth=rf_max_depth, min_samples_split=rf_min_samples_split, min_samples_leaf=rf_min_samples_leaf)
-    rf_model.fit(x_train, y_train)
-    y_pred = rf_model.predict(x_val)
-    precision = precision_score(y_val, y_pred, average='macro')
-    return precision
-
 if __name__ == '__main__':
-    for x_train, y_train, x_val, y_val, dataset_name in datasets:
-        # Create Optuna study for each datasets with MySQL storage
+    # Train for each datasets
+    for x_train, y_train, x_val, y_test, dataset_name in datasets:
+        
