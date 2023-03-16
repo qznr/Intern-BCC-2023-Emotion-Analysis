7efc48e0099d2f9dbf48b0efa65570f2eec280c0
qznr <mgustav.penting@gmail.com>
2023-03-16 20:11:22 +0700

    add test datasets

--- Datasets.py
+++ Datasets.py
@@ -1,4 +1,4 @@
-from Tfidf import y_val, x_val_vec_negations, x_val_vec_clean, y_train_clean
+from Tfidf import y_val, y_test, x_val_vec_negations, x_val_vec_clean, y_train_clean
 from Sampling import *
 import pickle
 import os
@@ -15,10 +15,34 @@ datasets = [
     (X_train_nm_clean, y_train_nm_clean, x_val_vec_clean, y_val, "NearMiss Clean")
     ]
 
+datasets_test = [
+    (X_train_adasyn_negations, y_train_adasyn_negations, x_val_vec_negations, y_test, "ADASYN Negations"),
+    (X_train_adasyn_clean, y_train_adasyn_clean, x_val_vec_clean, y_test, "ADASYN Clean"),
+    (x_train_vec_negations, y_train_clean, x_val_vec_negations, y_test, "Original Negations"),
+    (x_train_vec_clean, y_train_clean, x_val_vec_clean, y_test, "Original Clean"),
+    (X_train_ncr_negations, y_train_ncr_negations, x_val_vec_negations, y_test, "NCR Negations"),
+    (X_train_ncr_clean, y_train_ncr_clean, x_val_vec_clean, y_test, "NCR Clean"),
+    (X_train_nm_negations, y_train_nm_negations, x_val_vec_negations, y_test, "NearMiss Negations"),
+    (X_train_nm_clean, y_train_nm_clean, x_val_vec_clean, y_test, "NearMiss Clean")
+    ]
+if not os.path.exists("Datasets/Test"):
+    os.mkdir("Datasets/Test")
+if not os.path.exists("Datasets/Val"):
+    os.mkdir("Datasets/Val")
+
 # Check if each dataset exists, load from file if it does, save and load if it doesn't
 for i, dataset in enumerate(datasets):
     dataset_name = dataset[4]
-    file_name = f"Datasets/{dataset_name}.pkl"
+    file_name = f"Datasets/Test/{dataset_name}.pkl"
+    if not os.path.exists(file_name):
+        print(f"Saving {dataset_name} Sampled Object...")
+        with open(file_name, 'wb') as f:
+            pickle.dump(dataset, f)
+
+# Check if each dataset exists, save if it doesn't
+for i, dataset in enumerate(datasets):
+    dataset_name = dataset[4]
+    file_name = f"Datasets/Val/{dataset_name}.pkl"
     if not os.path.exists(file_name):
         print(f"Saving {dataset_name} Sampled Object...")
         with open(file_name, 'wb') as f:
