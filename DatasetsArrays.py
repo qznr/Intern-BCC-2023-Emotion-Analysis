b3c24c4e7ef1c95a441bb9199f7eea5249aa1aaf
qznr <mgustav.penting@gmail.com>
2023-03-17 07:53:22 +0700

    Nb Train final scores

--- DatasetsArrays.py
+++ DatasetsArrays.py
@@ -6,12 +6,12 @@ import scipy
 
 # del adasyn_clean, adasyn_negations, ncr_clean, ncr_negations, le_y, ncr_negations, nm_negations, nm_clean
 datasets = [
-    # (X_train_adasyn_negations, y_train_adasyn_negations, x_val_vec_negations, y_val, "ADASYN Negations"),
-    # (X_train_adasyn_clean, y_train_adasyn_clean, x_val_vec_clean, y_val, "ADASYN Clean"),
-    # (x_train_vec_negations, y_train_clean, x_val_vec_negations, y_val, "Original Negations"),
-    # (x_train_vec_clean, y_train_clean, x_val_vec_clean, y_val, "Original Clean"),
-    # (X_train_ncr_negations, y_train_ncr_negations, x_val_vec_negations, y_val, "NCR Negations"),
-    # (X_train_ncr_clean, y_train_ncr_clean, x_val_vec_clean, y_val, "NCR Clean"),
+    (X_train_adasyn_negations, y_train_adasyn_negations, x_val_vec_negations, y_val, "ADASYN Negations"),
+    (X_train_adasyn_clean, y_train_adasyn_clean, x_val_vec_clean, y_val, "ADASYN Clean"),
+    (x_train_vec_negations, y_train_clean, x_val_vec_negations, y_val, "Original Negations"),
+    (x_train_vec_clean, y_train_clean, x_val_vec_clean, y_val, "Original Clean"),
+    (X_train_ncr_negations, y_train_ncr_negations, x_val_vec_negations, y_val, "NCR Negations"),
+    (X_train_ncr_clean, y_train_ncr_clean, x_val_vec_clean, y_val, "NCR Clean"),
     (X_train_nm_negations, y_train_nm_negations, x_val_vec_negations, y_val, "NearMiss Negations"),
     (X_train_nm_clean, y_train_nm_clean, x_val_vec_clean, y_val, "NearMiss Clean")
     ]
